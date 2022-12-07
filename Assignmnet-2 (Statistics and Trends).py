3f52fb16a71932e3f1604e69f85b22bb2fb72102
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2022-12-07 19:36:29 +0000

    Calculating Mean
    Mean of Nitrous oxide emissions (% change from 1990) for 5 countries

--- Assignmnet-2 (Statistics and Trends).py
+++ Assignmnet-2 (Statistics and Trends).py
@@ -43,8 +43,10 @@ def read_data_file(input_file_name,countries):
 #calling the function with the data set and our own selection of countries.
 df_countries,df_years = read_data_file('API_19_DS2_en_csv_v2_4700503-Copy.csv',['Australia','Bolivia','Canada','Switzerland','Denmark'])
 
-#printing the statistical property "mean" using numpy module
-print('Mean:\n',np.mean(df_years.iloc[4:,10:20]))
+#printing "mean" using numpy module for 5 countries
+print('Calculating Mean of "Nitrous oxide emissions (% change from 1990)" for 5 countries')
+print('----------------------------------------------------------------------------------')
+print(np.mean(df_years.iloc[4:,[29,105,181,257,333]]))
 
 #printing the statistical property "standard deviation" using numpy module
 print('\n\nStandard Deviation:\n',np.std(df_years.iloc[5:,10:20]))
