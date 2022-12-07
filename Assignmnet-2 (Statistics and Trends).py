965a12414a5493def532bf1c42e704deec996011
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2022-12-07 14:28:22 +0000

    Importing python modules
    Importing pandas, numpy, matplotlib and scipy modules

---
+++ Assignmnet-2 (Statistics and Trends).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 16:20:54 2022
+
+@author: sande
+"""
+
+#importing pandas module to read the file(data set) and to calculate the statistical property(Describe)
+import pandas as pd
+
+#importing numpy module to calculate the statistical properties (mean and standard deviation)
+import numpy as np
+
+#importing pyplot from matplotlib module to plot the visualization graphs
+import matplotlib.pyplot as plt
+
+#importing stats from scipy module to calculate the statistical properties (kurtosis and skewness)
+import scipy.stats as st
+
